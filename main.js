ab006275355780696e02ad5b2e86cb09a19035e6
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2021-10-20 01:20:57 +0100

    :sparkles: Payment Methods, Credit Plans, Addresses and Wallet added

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
-const getSnapshot = () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
-    const pageUrl = tab[0].url
-    const regex = /([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/g
-
-    const matchArray = pageUrl.match(regex)
-    const domain = pageUrl.split('/')[2]
-
-    if (!matchArray) {
-      chrome.tabs.create({
-        url: 'https://media.giphy.com/media/ji6zzUZwNIuLS/giphy.gif?cid=ecf05e47xmmukjdmwahuqizbjq5r73vmxiys1xbfz9346exs&rid=giphy.gif&ct=g'
-      })
-      return
-    }
-
-    chrome.tabs.create({ url: `http://${domain}/checkout-api-v2/checkouts/${matchArray[0]}` })
+const handleError = () => {
+  chrome.tabs.create({
+    url: 'https://media.giphy.com/media/ji6zzUZwNIuLS/giphy.gif?cid=ecf05e47xmmukjdmwahuqizbjq5r73vmxiys1xbfz9346exs&rid=giphy.gif&ct=g'
   })
 }
 
-chrome.contextMenus.create({
-  title: '_Open Snapshot 📸',
-  contexts: ['all'],
-  onclick: getSnapshot,
-  documentUrlPatterns: [
-    'http://localhost/checkout/*',
-    'http://ui-payment-stub.service.eu-west-1.dev.deveng.systems/checkout/*',
-    'https://uat2.test-arg-uk.com/checkout/*',
-    'https://www.argos.co.uk/checkout/*'
-  ]
-})
+const checkoutDocumentUrlPatterns = [
+  'http://localhost/checkout/*',
+  'http://ui-payment-stub.service.eu-west-1.dev.deveng.systems/checkout/*',
+  'https://uat2.test-arg-uk.com/checkout/*',
+  'https://www.argos.co.uk/checkout/*'
+]
 
 const getOrder = () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
@@ -39,9 +21,7 @@ const getOrder = () => {
     const id = pageUrl.split('/')[5]
 
     if (!matchArray) {
-      chrome.tabs.create({
-        url: 'https://media.giphy.com/media/ji6zzUZwNIuLS/giphy.gif?cid=ecf05e47xmmukjdmwahuqizbjq5r73vmxiys1xbfz9346exs&rid=giphy.gif&ct=g'
-      })
+      handleError()
       return
     }
 
@@ -59,6 +39,111 @@ chrome.contextMenus.create({
     'https://uat2.test-arg-uk.com/confirmation/*',
     'https://www.argos.co.uk/confirmation/*'
   ]
+})
+
+const endpointBuilder = (tab) => {
+  const pageUrl = tab[0].url
+  const regex = /([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/g
+
+  const matchArray = pageUrl.match(regex)
+  const domain = pageUrl.split('/')[2]
+
+  if (!matchArray) {
+    handleError()
+    return
+  }
+
+  return { domain, id: matchArray[0] }
+}
+
+const getSnapshot = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+    const { domain, id } = endpointBuilder(tab)
+    chrome.tabs.create({ url: `http://${domain}/checkout-api-v2/checkouts/${id}` })
+  })
+}
+
+const getPaymentMethods = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+    const { domain, id } = endpointBuilder(tab)
+    chrome.tabs.create({ url: `http://${domain}/checkout-api-v2/checkouts/${id}/paymentMethods` })
+  })
+}
+
+const getCreditPlans = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+    const { domain, id } = endpointBuilder(tab)
+    chrome.tabs.create({ url: `http://${domain}/checkout-api-v2/checkouts/${id}/creditplans` })
+  })
+}
+
+const getAddresses = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+    const { domain, id } = endpointBuilder(tab)
+    try {
+      fetch(`http://${domain}/checkout-api-v2/checkouts/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.snapshot.customer.id)
+          const customerId = data.snapshot.customer.id
+          chrome.tabs.create({ url: `http://${domain}/account-api/users/${customerId}/addresses` })
+        })
+    } catch {
+      handleError()
+    }
+  })
+}
+
+const getWallet = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+    const { domain, id } = endpointBuilder(tab)
+    try {
+      fetch(`http://${domain}/checkout-api-v2/checkouts/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.snapshot.customer.id)
+          const customerId = data.snapshot.customer.id
+          chrome.tabs.create({ url: `http://${domain}/account-api/users/${customerId}/wallet` })
+        })
+    } catch {
+      handleError()
+    }
+  })
+}
+
+chrome.contextMenus.create({
+  title: 'Open Snapshot 📸',
+  contexts: ['all'],
+  onclick: getSnapshot,
+  documentUrlPatterns: checkoutDocumentUrlPatterns
+})
+
+chrome.contextMenus.create({
+  title: 'Open Payment Methods 💰',
+  contexts: ['all'],
+  onclick: getPaymentMethods,
+  documentUrlPatterns: checkoutDocumentUrlPatterns
+})
+
+chrome.contextMenus.create({
+  title: 'Open Credit Plans 💳',
+  contexts: ['all'],
+  onclick: getCreditPlans,
+  documentUrlPatterns: checkoutDocumentUrlPatterns
+})
+
+chrome.contextMenus.create({
+  title: 'Open Addresses 🏠',
+  contexts: ['all'],
+  onclick: getAddresses,
+  documentUrlPatterns: checkoutDocumentUrlPatterns
+})
+
+chrome.contextMenus.create({
+  title: 'Open Wallet 🧧',
+  contexts: ['all'],
+  onclick: getWallet,
+  documentUrlPatterns: checkoutDocumentUrlPatterns
 })
 
 chrome.browserAction.onClicked.addListener(function () {
